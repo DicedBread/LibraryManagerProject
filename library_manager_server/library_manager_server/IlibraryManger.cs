a5142a8d53e7d0605720ba9cc0f217a6c62c02e0
DicedBread <benjijread@gmail.com>
2024-11-25 17:26:49 +1300

    abstracted lib manager

---
+++ library_manager_server/library_manager_server/IlibraryManger.cs
@@ -0,0 +1,14 @@
+﻿using library_manager_server.model;
+using Microsoft.AspNetCore.Identity;
+
+namespace library_manager_server
+{
+    public interface ILibraryManger
+    {
+        public List<Book> GetBooks(int limit, int offset);
+        public Book? GetBook(string isbn);
+
+        public PasswordVerificationResult AuthenticateUser(string username, string password);
+
+    }
+}
---
+++ library_manager_server/library_manager_server_tests/BookControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using library_manager_server.model;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library_manager_server_tests
+{
+    internal class BookControllerTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Book testOut = new Book() { Authour = "jim", Id = "okwefkm", ImgUrl = "asdf", Title = "asd" };
+
+
+            var mockDB = new Mock<LibrayManager>();
+                mockDB.Setup(m => m.GetBook("jim")).Returns(testOut);
+
+
+            Assert.False(true);
+        }
+    }
+}
