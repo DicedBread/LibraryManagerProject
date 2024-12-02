3ee8cf573e0481dfd20c460e35cd303fc2410fe8
DicedBread <benjijread@gmail.com>
2024-12-02 17:26:55 +1300

    loans endpoint structure added

---
+++ library_manager_server/library_manager_server/Controllers/Loans.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace library_manager_server;
+
+[ApiController]
+[Route("api/[controller]")]
+public class Loans : ControllerBase
+{
+    LibraryManager libraryManager;
+    public Loans(LibraryManager libraryManager) => this.libraryManager = libraryManager;
+
+    [HttpGet()]
+    public async Task<IActionResult> GetLoans()
+    {
+        return NotFound();
+    }
+
+    [HttpPost()]
+    public async Task<IActionResult> CreateLoan()
+    {
+        
+        return NotFound();
+    }
+
+    [HttpDelete()]
+    public async Task<IActionResult> DeleteLoans()
+    {
+        
+        return NotFound();
+    }
+        
+    
+    
+}
