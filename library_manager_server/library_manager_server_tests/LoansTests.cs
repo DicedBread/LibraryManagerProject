2b6a71eb27c1902b5ae13d7fb60ee78b20f2f1a5
DicedBread <benjijread@gmail.com>
2024-12-15 15:19:57 +1300

    getLoans tests

--- library_manager_server/library_manager_server_tests/LoansTests.cs
+++ library_manager_server/library_manager_server_tests/LoansTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Runtime.InteropServices.JavaScript;
 using System.Security.Claims;
 using library_manager_server;
 using library_manager_server.model;
@@ -17,62 +18,67 @@ public class LoansTests
 {
     private const double TestUserId = 1;
     private const string TestSessionId = "1";
-    
-    Mock<ISessionHandler> sessionHandlerMock;
-    Mock<ILibraryManager> libraryManagerMock;
-    ControllerContext controllerContext;
-    Loan testLoan;
+    private const string TestBookISBN = "12ab"; 
+
+    private Mock<ISessionHandler> _sessionHandlerMock;
+    private Mock<ILibraryManager> _libraryManagerMock;
+    private ControllerContext _controllerContext;
+    private List<Loan> _loans = new List<Loan>();
+    private Loan _testLoan;
     
     [SetUp]
     public void Setup()
     {
-        List<Loan> loans = new List<Loan>();
+        _loans.Clear();
         for (int i = 0; i < 10; i++)
         {
-            loans.Add(new Loan()
+            _loans.Add(new Loan()
             {
-                Loan_id = i,
+                LoanId = i,
+                UserId = TestUserId,
                 Isbn = "a" + i.ToString(),
                 Date = new DateTime(2017, 1, i + 1),
             });
         }
-        testLoan = loans[0];
+        _testLoan = _loans[0];
         
-        sessionHandlerMock = new Mock<ISessionHandler>();
-        libraryManagerMock = new Mock<ILibraryManager>();
-        controllerContext = new ControllerContext()
+        _sessionHandlerMock = new Mock<ISessionHandler>();
+        _libraryManagerMock = new Mock<ILibraryManager>();
+        _controllerContext = new ControllerContext()
         {
             HttpContext = new DefaultHttpContext()
         };
         
-        sessionHandlerMock.Setup(m => m.IsActiveSession(TestSessionId)).Returns(true);
-        sessionHandlerMock.Setup(m => m.GetUserId(TestSessionId)).Returns(TestUserId);
-        sessionHandlerMock.Setup(m => m.GetSession(It.IsAny<HttpContext>())).Returns(TestSessionId);
+        _sessionHandlerMock.Setup(m => m.IsActiveSession(TestSessionId)).Returns(true);
+        _sessionHandlerMock.Setup(m => m.GetUserId(TestSessionId)).Returns(TestUserId);
+        _sessionHandlerMock.Setup(m => m.GetSession(It.IsAny<HttpContext>())).Returns(TestSessionId);
         
-        libraryManagerMock.Setup(m => m.GetLoan(TestUserId)).Returns(testLoan);
-        libraryManagerMock.Setup(m => m.OwnsLoan(1, TestUserId)).Returns(true);
+        _libraryManagerMock.Setup(m => m.GetLoan(TestUserId)).Returns(_testLoan);
+        _libraryManagerMock.Setup(m => m.OwnsLoan(1, TestUserId)).Returns(true);
         
-        libraryManagerMock.Setup(m => m.GetLoans(TestUserId)).Returns(loans);
+        _libraryManagerMock.Setup(m => m.GetLoans(TestUserId)).Returns(_loans);
+        
+        // _libraryManagerMock.Setup(m => m.CreateLoan(TestBookISBN, TestUserId, It.IsAny<DateTime>())).Returns(null);
     }
     
     [Test]
     public async Task GetLoan_Valid()
     {
-        Loans controller = new Loans(libraryManagerMock.Object, NullLogger<Loans>.Instance, sessionHandlerMock.Object);
-        controller.ControllerContext = controllerContext;
+        Loans controller = new Loans(_libraryManagerMock.Object, NullLogger<Loans>.Instance, _sessionHandlerMock.Object);
+        controller.ControllerContext = _controllerContext;
         ActionResult<Loan> ret = await controller.GetLoan(1);
         
         Assert.IsNotNull(ret.Value);
-        Assert.That(testLoan, Is.EqualTo(ret.Value)); 
+        Assert.That(_testLoan, Is.EqualTo(ret.Value)); 
     }
 
     [Test]
     public async Task GetLoan_NoSession()
     {
-        sessionHandlerMock.Setup(m => m.GetSession(It.IsAny<HttpContext>())).Returns((string?) null); // no session in http context
+        _sessionHandlerMock.Setup(m => m.GetSession(It.IsAny<HttpContext>())).Returns((string?) null); // no session in http context
         
-        Loans controller = new Loans(libraryManagerMock.Object, NullLogger<Loans>.Instance, sessionHandlerMock.Object);
-        controller.ControllerContext = controllerContext;
+        Loans controller = new Loans(_libraryManagerMock.Object, NullLogger<Loans>.Instance, _sessionHandlerMock.Object);
+        controller.ControllerContext = _controllerContext;
         ActionResult<Loan> ret = await controller.GetLoan(1);
         
         UnauthorizedResult? returnValue = ret.Result as UnauthorizedResult;
@@ -83,11 +89,11 @@ public class LoansTests
     [Test]
     public async Task GetLoan_DeadSession()
     {
-        sessionHandlerMock.Setup(m => m.IsActiveSession(It.IsAny<string>())).Returns(false); // dead session 
-        sessionHandlerMock.Setup(m => m.GetUserId(It.IsAny<string>())).Returns((double?) null); // dead session
+        _sessionHandlerMock.Setup(m => m.IsActiveSession(It.IsAny<string>())).Returns(false); // dead session 
+        _sessionHandlerMock.Setup(m => m.GetUserId(It.IsAny<string>())).Returns((double?) null); // dead session
         
-        Loans controller = new Loans(libraryManagerMock.Object, NullLogger<Loans>.Instance, sessionHandlerMock.Object);
-        controller.ControllerContext = controllerContext;
+        Loans controller = new Loans(_libraryManagerMock.Object, NullLogger<Loans>.Instance, _sessionHandlerMock.Object);
+        controller.ControllerContext = _controllerContext;
         ActionResult<Loan> ret = await controller.GetLoan(1);
         
         UnauthorizedResult? returnValue = ret.Result as UnauthorizedResult;
@@ -98,10 +104,10 @@ public class LoansTests
     [Test]
     public async Task GetLoan_DoesNotOwnLoan()
     {
-        libraryManagerMock.Setup(m => m.OwnsLoan(1, 1)).Returns(false); // does not own loan 
+        _libraryManagerMock.Setup(m => m.OwnsLoan(1, 1)).Returns(false); // does not own loan 
         
-        Loans controller = new Loans(libraryManagerMock.Object, NullLogger<Loans>.Instance, sessionHandlerMock.Object);
-        controller.ControllerContext = controllerContext;
+        Loans controller = new Loans(_libraryManagerMock.Object, NullLogger<Loans>.Instance, _sessionHandlerMock.Object);
+        controller.ControllerContext = _controllerContext;
         ActionResult<Loan> ret = await controller.GetLoan(1);
         
         ForbidResult? returnValue = ret.Result as ForbidResult; // forbid res does not contain status code 
@@ -111,15 +117,55 @@ public class LoansTests
     
     [Test]
     public async Task GetLoan_LoanDoesNotExist(){
-        libraryManagerMock.Setup(m => m.GetLoan(1)).Returns((Loan?) null); // no loan
+        _libraryManagerMock.Setup(m => m.GetLoan(1)).Returns((Loan?) null); // no loan
         
-        Loans controller = new Loans(libraryManagerMock.Object, NullLogger<Loans>.Instance, sessionHandlerMock.Object);
-        controller.ControllerContext = controllerContext;
+        Loans controller = new Loans(_libraryManagerMock.Object, NullLogger<Loans>.Instance, _sessionHandlerMock.Object);
+        controller.ControllerContext = _controllerContext;
         ActionResult<Loan> ret = await controller.GetLoan(1);
 
         BadRequestResult? returnValue = ret.Result as BadRequestResult;
         Assert.IsNotNull(returnValue);
         Assert.That(returnValue.StatusCode, Is.EqualTo((int) HttpStatusCode.BadRequest));
+    }
+
+    [Test]
+    public async Task GetLoans_Valid()
+    {
+        Loans controller = new Loans(_libraryManagerMock.Object, NullLogger<Loans>.Instance, _sessionHandlerMock.Object);
+        controller.ControllerContext = _controllerContext;
+        ActionResult<List<Loan>> ret = await controller.GetLoans();
+        
+        Assert.IsNotNull(ret.Value);
+        Assert.That(_loans, Is.EqualTo(ret.Value)); 
+    }
+
+    [Test]
+    public async Task GetLoans_DeadSession()
+    {
+        _sessionHandlerMock.Setup(m => m.IsActiveSession(It.IsAny<string>())).Returns(false);
+        _sessionHandlerMock.Setup(m => m.GetUserId(It.IsAny<string>())).Returns((double?) null);
+        
+        Loans controller = new Loans(_libraryManagerMock.Object, NullLogger<Loans>.Instance, _sessionHandlerMock.Object);
+        controller.ControllerContext = _controllerContext;
+        ActionResult<List<Loan>> ret = await controller.GetLoans();
+        
+        UnauthorizedResult? returnValue = ret.Result as UnauthorizedResult;
+        Assert.IsNotNull(returnValue);
+        Assert.That(returnValue.StatusCode, Is.EqualTo((int) HttpStatusCode.Unauthorized));
+    }
+
+    [Test]
+    public async Task GetLoans_NoSession()
+    {
+        _sessionHandlerMock.Setup(m => m.GetSession(It.IsAny<HttpContext>())).Returns((string?) null);
+        
+        Loans controller = new Loans(_libraryManagerMock.Object, NullLogger<Loans>.Instance, _sessionHandlerMock.Object);
+        controller.ControllerContext = _controllerContext;
+        ActionResult<List<Loan>> ret = await controller.GetLoans();
+        
+        UnauthorizedResult? returnValue = ret.Result as UnauthorizedResult;
+        Assert.IsNotNull(returnValue);
+        Assert.That(returnValue.StatusCode, Is.EqualTo((int) HttpStatusCode.Unauthorized));
     }
 
 
