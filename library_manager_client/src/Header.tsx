174da21ffa5e4f6511604d47853c12cdc5bcf74d
DicedBread <benjijread@gmail.com>
2025-01-08 15:25:33 +1300

    need to rename cookie and get something to validate cookie

--- library_manager_client/src/Header.tsx
+++ library_manager_client/src/Header.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style/App.css"
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 function Header(){
+    const [cookies] = useCookies([".AspNetCore.Cookies"]);
+    // const [isLoggedIn, setIsloggedIn] = useState<boolean>(!!cookies['.AspNetCore.Cookies']);
+    const nav = useNavigate();
+    
     return (
         <div className="header">
             <h1>Library</h1>
+            <p>{cookies[".AspNetCore.Cookies"]}</p>
+            <div className="nav">
+                <button onClick={() => nav("/")}>Home</button>
+                {/* {isLoggedIn && (
+                    <p>yeo</p>
+                )} */}
+                <button onClick={() => nav("/login")}>Login</button>
+            </div>
         </div>
     );
 }
