609e23d6a964ce7e73ae01ca0dcb71859a6a6719
DicedBread <benjijread@gmail.com>
2025-02-04 14:55:28 +1300

    login refactored

--- library_manager_client/src/App.tsx
+++ library_manager_client/src/App.tsx
@@ -8,24 +8,35 @@ import Loans from './loans/Loans'
 import { RouteUrl } from './util/RouteUrl';
 import { createContext, useState } from 'react';
 import { loginStateContext } from './account/LoginStateContext';
+import { createTheme, CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
 
 
 
 function App() {
   const [ls, setLs] = useState<boolean>(false);
 
-  return (    
-    <BrowserRouter>
-      <loginStateContext.Provider value={{ LoggedIn: ls, setLoggedIn: setLs }}>
-        <Header />
-        <Routes>
-          <Route path={RouteUrl.Home} element={<BookContent />} />
-          <Route path={RouteUrl.Login} element={<Login />}/>
-          <Route path={RouteUrl.Register} element={<Register />}/>
-          <Route path={RouteUrl.Loans} element={<Loans />}/>
-        </Routes>
-      </loginStateContext.Provider>
-    </BrowserRouter>
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <loginStateContext.Provider value={{ LoggedIn: ls, setLoggedIn: setLs }}>
+          <Header />
+          <Routes>
+            <Route path={RouteUrl.Home} element={<BookContent />} />
+            <Route path={RouteUrl.Login} element={<Login />} />
+            <Route path={RouteUrl.Register} element={<Register />} />
+            <Route path={RouteUrl.Loans} element={<Loans />} />
+          </Routes>
+        </loginStateContext.Provider>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
--- library_manager_client/src/account/Login.tsx
+++ library_manager_client/src/account/Login.tsx
@@ -3,24 +3,26 @@ import "../style/App.css"
 import "../style/Form.css"
 import { redirect, useNavigate } from "react-router-dom";
 import { loginStateContext } from "./LoginStateContext";
+import { Box, Button, Stack, TextField } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import { blue } from "@mui/material/colors";
 
 
-
-interface Login{
-    email : string;
-    password : string;
+interface Login {
+    email: string;
+    password: string;
 }
 
-function Login(){
+function Login() {
     const [inputs, setInputs] = useState<Login>({ email: "", password: "" });
     const [failedLogin, setFailedLogin] = useState<boolean>(false);
     const nav = useNavigate();
-    const {LoggedIn, setLoggedIn} = useContext(loginStateContext);
-    
+    const { LoggedIn, setLoggedIn } = useContext(loginStateContext);
+
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         setFailedLogin(false);
-        const url:string = `${import.meta.env.VITE_SERVER_DOMAIN}/api/Account/login`;
+        const url: string = `${import.meta.env.VITE_SERVER_DOMAIN}/api/Account/login`;
         console.log(url)
         fetch(url, {
             method: 'POST',
@@ -31,56 +33,66 @@ function Login(){
             },
             credentials: "include"
         })
-        .then(response => {
-            if (response.ok) {
-                setLoggedIn(true);
-                nav(-1);
-            } else {
-                setLoggedIn(false);
-                setFailedLogin(true);
-            }
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+            .then(response => {
+                if (response.ok) {
+                    setLoggedIn(true);
+                    nav(-1);
+                } else {
+                    setLoggedIn(false);
+                    setFailedLogin(true);
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     }
 
-    const handleChange = (event : ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = event.target
-        setInputs(prevInputs => ({...prevInputs, [name]: value}));
-    } 
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target
+        setInputs(prevInputs => ({ ...prevInputs, [name]: value }));
+    }
 
     return (
-        <div className="formBase">
-        <form className="" onSubmit={handleSubmit}>
-            <label htmlFor="email" className="formlabel">
-                Email <br />
-                <input 
-                    type="text" 
-                    name="email"
-                    value={inputs?.email || ""}
-                    onChange={handleChange}
-                    required
-                />
-            </label>
+        <Grid
+            container
+            direction={"column"}
+            justifyContent={"center"}
+            padding={20}
+            justifyItems={"center"}
+            alignItems={"center"}
+        >
+            <Grid>
+                <form onSubmit={handleSubmit}>
+                    <Stack
+                        border={1}
+                        color={"secondary"}
+                        padding={5}
+                        borderRadius={2}
+                        spacing={1}
+                    >
+                        <TextField
+                            color={"primary"}
+                            type={"email"}
+                            label={"email"} 
+                            name={"email"}
+                            value={inputs?.email || ""}
+                            onChange={handleChange}
+                            required
+                        />
+                        <TextField 
+                            type={"password"} 
+                            label={"password"} 
+                            name={"password"}
+                            value={inputs?.password || ""} 
+                            onChange={handleChange}
+                            required
+                        />
+                        <Button type="submit" variant="contained">Login</Button>
+                    </Stack>
 
-            <label htmlFor="password" className="formlabel">
-                Password <br /> 
-                <input 
-                    type="password"
-                    name="password"
-                    value={inputs?.password || ""}
-                    onChange={handleChange}    
-                    required
-                />
-            </label>
-
-            {failedLogin && (
-                <p>Invalid email or password</p>
-            )}
-            <button type="submit">Login</button>
-        </form>
-        </div>
+                </form>
+            </Grid>
+        </Grid>
     );
 }
 
