657be505fa1553676cdc16733fbaa7855bdae716
DicedBread <benjijread@gmail.com>
2025-03-05 23:00:04 +1300

    scaffold

---
+++ library_manager_server/library_manager_server_virtual_client/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace library_manager_server_virtual_client;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
---
+++ library_manager_server/library_manager_server_virtual_client/VirtualClient.cs
@@ -0,0 +1,12 @@
+namespace library_manager_server_virtual_client;
+
+
+class VirtualClient{
+    
+    public VirtualClient(){
+    }   
+
+    public void Run(){
+        
+    } 
+}
